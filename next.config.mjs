f4d89c7996a40d277d77db99ddd06d47925639aa
Techie_Steve <tuwa.1simon@gmail.com>
2025-12-26 17:00:56 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/index.html',
+        permanent: true, // or false if you prefer 302
+      },
+    ];
+  },
 };
 
 export default nextConfig;
